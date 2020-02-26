3be2e49638971fbbb43372d3aa471ad5879cdac0
Brandon Gigous <bgigous@boulderimaging.com>
2020-02-26 16:18:17 -0700

    Add password functions for creating and verifying hashes and password property to Fan model, + UserMixin. Init for auth blueprint, LoginForm.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,5 @@
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 from ragtime import db
 
 class Role(db.Model):
@@ -10,16 +12,28 @@ class Role(db.Model):
         return f'<Role {self.name}>'
 
 
-class Fan(db.Model):
+class Fan(UserMixin, db.Model):
     __tablename__ = 'fans'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     #email = db.Column(db.String(64), unique=True, index=True)
+    password_hash = db.Column(db.String(128))
     #password = db.Column(db.String(64), unique=True, index=True)
 
     def __repr__(self):
         return f'<Fan {self.username}>'
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readable attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 """
