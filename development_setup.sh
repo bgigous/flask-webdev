95417cc9b5505711f69ec08f048375636125797d
gigous <bgigous@protonmail.com>
2020-03-16 21:01:32 -0600

    Fix development_setup.sh

--- development_setup.sh
+++ development_setup.sh
@@ -2,4 +2,4 @@ export FLASK_APP=ragtime.py
 export FLASK_DEBUG=1
 export FLASK_CONFIG=development
 export FLASK_ENV=development
-export DATABASE_TEST_URL=sqlite:////home/brandon/git_repos/ragtime/data-test.sqlite
+export DATABASE_TEST_URL=sqlite:///$PWD/data-test.sqlite
